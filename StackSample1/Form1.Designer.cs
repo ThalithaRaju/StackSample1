3cbb10b451f1a6e58c2ef0dd3b01f9c271b7a4c2
ThalithaRaju <thalitharaju22@gmail.com>
2020-10-07 18:06:53 +0530

    stack commit1

---
+++ StackSample1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace StackSample1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtOne = new System.Windows.Forms.TextBox();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtOne
+            // 
+            this.txtOne.Location = new System.Drawing.Point(12, 26);
+            this.txtOne.Name = "txtOne";
+            this.txtOne.Size = new System.Drawing.Size(308, 20);
+            this.txtOne.TabIndex = 0;
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(15, 66);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(148, 23);
+            this.btnPush.TabIndex = 1;
+            this.btnPush.Text = "Push";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(181, 66);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(139, 23);
+            this.btnPop.TabIndex = 2;
+            this.btnPop.Text = "Pop";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(12, 111);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(308, 160);
+            this.lstBox.TabIndex = 3;
+            this.lstBox.SelectedIndexChanged += new System.EventHandler(this.lstBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter the text";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(332, 292);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.txtOne);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOne;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ StackSample1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StackSample1
+{
+    public partial class Form1 : Form
+    {
+        Stack<string> names = new Stack<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            names.Push(txtOne.Text);
+            lstBox.Items.Add(txtOne.Text);
+            txtOne.Text = string.Empty;
+        }
+
+        private void lstBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtOne.Text = lstBox.SelectedItem.ToString();
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            if (names.Count!=0)
+            {
+                lstBox.Items.Remove(names.Pop());
+            }
+            else
+            {
+                MessageBox.Show("Stack is empty");
+            }
+        }
+    }
+}
